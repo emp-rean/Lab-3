eaca199dbb8cff5a65d2ef23efbad3eeccbcd095
emp-rean <spforimport@gmail.com>
2024-10-13 16:38:23 +0200

    Update Lab-3.cpp

--- Lab-3.cpp
+++ Lab-3.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
+#include <bitset>
 
-int main() {
+using std::cin;
+using std::cout;
+using std::bitset;
 
+int main() 
+{   // Ввод значений
+    int x, i;
+    cout << "x = ";
+    cin >> x;
+    cout << "i = ";
+    cin >> i;
+
+    // Перевод в двоичную
+    bitset<32> y (x);
+    // Замена i-го бита на 0
+    y = y.reset(i - 1);
+    // Перевод в десятичную
+    cout << y.to_ulong();
 }
